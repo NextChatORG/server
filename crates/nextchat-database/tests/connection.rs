2b6902d3f07c91f8d99b1841057ca36a79650692
Daniel Solarte <danielsolartech@hotmail.com>
2021-03-23 13:17:32 -0500

    fix: NextChat Database test

--- crates/nextchat-database/tests/connection.rs
+++ crates/nextchat-database/tests/connection.rs
@@ -1,3 +1,5 @@
+use std::env;
+
 use nextchat_database::get_client;
 
 #[tokio::test]
@@ -5,6 +7,12 @@ use nextchat_database::get_client;
     expected = "Database Error -> Cannot read `DATABASE_URL` from environment variables."
 )]
 async fn test_database_url_panic() {
+    if env::var("DATABASE_URL").is_ok() {
+        env::remove_var("DATABASE_URL");
+    }
+
+    assert!(env::var("DATABASE_URL").is_err());
+
     let client = get_client().await;
     assert!(client.is_err());
 }
