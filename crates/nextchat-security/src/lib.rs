3c258b83800825f32cf9c9c7ce28aae08462e667
Daniel Solarte <danielsolartech@hotmail.com>
2021-03-23 10:11:21 -0500

    feat: Add NextChat Security tests

--- crates/nextchat-security/src/lib.rs
+++ crates/nextchat-security/src/lib.rs
@@ -1,34 +1,39 @@
 //! NextChat Security library.
 //!
 //! This module contains the encrypt function for the user password using
-//! Argon2 algorithm and the app secret key. Also, contains the function
+//! Argon2id algorithm and the app secret key. Also, contains the function
 //! to verify the user password using the posible password and the
 //! encrypted password hash.
 
 use std::env;
 
 use argonautica::{Hasher, Verifier};
-use colored::*;
 
 /// Get the `APP_SECRET_KEY` variable from the environment.
 fn get_secret_key() -> String {
     match env::var("APP_SECRET_KEY") {
         Ok(secret_key) => secret_key,
         Err(_) => {
-            panic!(
-                "{} Cannot get the `APP_SECRET_KEY` variable.",
-                "Security Error".red()
-            );
+            panic!("Security Error -> Cannot get the `APP_SECRET_KEY` variable.");
         }
     }
 }
 
-/// Encrypt an user password using Argon2 algorithm and the app secret key.
+/// Encrypt an user password using Argon2id algorithm and the app secret key.
 ///
 /// # Example
 /// ```rust
-/// let password: &str = "1234";
-/// let hash: String = match encrypt_password(password);
+/// use std::env;
+///
+/// use nextchat_security::encrypt_password;
+///
+/// fn main() {
+///     env::set_var("APP_SECRET_KEY", "NextChatORG");
+///
+///     let password: &str = "1234";
+///     let hash = encrypt_password(password);
+///     assert!(hash.is_ok());
+/// }
 /// ```
 pub fn encrypt_password(password: &str) -> Result<String, String> {
     let mut hasher = Hasher::default();
@@ -39,18 +44,30 @@ pub fn encrypt_password(password: &str) -> Result<String, String> {
     {
         Ok(password_hash) => Ok(password_hash),
         Err(_) => Err(String::from(
-            "Cannot encrypt the password using Argon2 algorithm.",
+            "Cannot encrypt the password using Argon2id algorithm.",
         )),
     }
 }
 
-/// Verify the password with the user password hash using Argon2 algorithm and the
+/// Verify the password with the user password hash using Argon2id algorithm and the
 /// app secret key.
 ///
 /// # Example
 /// ```rust
-/// let password: &str = "1234";
-/// let check: bool = verify_password(password, "argon2_hash");
+/// use std::env;
+///
+/// use nextchat_security::{encrypt_password, verify_password};
+///
+/// fn main() {
+///     env::set_var("APP_SECRET_KEY", "NextChatORG");
+///
+///     let password: &str = "1234";
+///     let hash: String = encrypt_password(password).unwrap();
+///
+///     let verification = verify_password(password, &hash);
+///     assert!(verification.is_ok());
+///     assert!(verification.unwrap());
+/// }
 /// ```
 pub fn verify_password(password: &str, password_hash: &str) -> Result<bool, String> {
     let mut verifier = Verifier::default();
@@ -62,7 +79,7 @@ pub fn verify_password(password: &str, password_hash: &str) -> Result<bool, Stri
     {
         Ok(result) => Ok(result),
         Err(_) => Err(String::from(
-            "Cannot verify the password hash using Argon2 algorithm.",
+            "Cannot verify the password hash using Argon2id algorithm.",
         )),
     }
 }
---
+++ crates/nextchat-security/tests/password.rs
@@ -0,0 +1,23 @@
+use std::env;
+
+use nextchat_security::{encrypt_password, verify_password};
+
+#[test]
+fn test_encryption() {
+    env::set_var("APP_SECRET_KEY", "NextChatORG");
+
+    let password = "1234";
+
+    let hash = encrypt_password(password);
+    assert!(hash.is_ok());
+
+    let verification = verify_password(password, &hash.unwrap());
+    assert!(verification.is_ok());
+    assert!(verification.unwrap());
+}
+
+#[test]
+#[should_panic(expected = "Security Error -> Cannot get the `APP_SECRET_KEY` variable.")]
+fn test_app_secret_key_panic() {
+    encrypt_password("1234").unwrap();
+}
